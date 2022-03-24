162b56cd9a0b13fad712b2fe8390ac438fc04930
Liis Normann <liis.normann@gmail.com>
2022-03-24 14:37:36 +0000

    Add primary key with ALTER TABLE

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ con.connect(function (err){
     if (err) throw err;
     console.log("Connected!");
 
-    var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+    var sql = "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY";
 
     con.query(sql, function (err, result) {
         if (err) throw err;
