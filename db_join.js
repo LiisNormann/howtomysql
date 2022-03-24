489f9d7d2e47fa612230e35ef3e4db5b7f9e7340
Liis Normann <liis.normann@gmail.com>
2022-03-24 16:15:09 +0000

    Left join

--- db_join.js
+++ db_join.js
@@ -10,7 +10,10 @@ var con = mysql.createConnection({
 con.connect(function(err) {
     if (err) throw err;
     /*Connect two tables by using one field from each table as the connection point:*/
-    var sql = "SELECT users.name AS user, products.name AS favorite FROM users JOIN products ON users.favorite_product = products.id";
+    var sql = "SELECT users.name AS user,\n" +
+        "products.name AS favorite\n" +
+        "FROM users\n" +
+        "LEFT JOIN products ON users.favorite_product = products.id";
     con.query(sql, function (err, result) {
         if (err) throw err;
         console.log(result);
