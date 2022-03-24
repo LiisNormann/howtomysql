bf5a0ff62ef3577606895a63e8d49fb423d9b4ec
Liis Normann <liis.normann@gmail.com>
2022-03-24 14:46:06 +0000

    INSERT single, INSERT multiple, INSERT id

--- db_insert_id.js
+++ db_insert_id.js
@@ -7,14 +7,11 @@ var con = mysql.createConnection({
     database: "mydb"
 });
 
-con.connect(function (err){
+con.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!");
-
-    var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
-
+    var sql = "INSERT INTO customers (name, address) VALUES ('Michelle', 'Blue Village 1')";
     con.query(sql, function (err, result) {
         if (err) throw err;
-        console.log("1 record inserted");
+        console.log("1 record inserted, ID: " + result.insertId);
     });
 });
