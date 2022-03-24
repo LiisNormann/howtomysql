bd531dc3e79b69e942fe683cc3cea8bb1c9723a7
Liis Normann <liis.normann@gmail.com>
2022-03-24 15:51:35 +0000

    Added tables users and favorite_products with inserted values

--- db_insert_id.js
+++ db_insert_id.js
@@ -9,7 +9,7 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
-    var sql = "INSERT INTO customers (name, address) VALUES ('Michelle', 'Blue Village 1')";
+    var sql = "INSERT INTO users (name, address) VALUES ('Michelle', 'Blue Village 1')";
     con.query(sql, function (err, result) {
         if (err) throw err;
         console.log("1 record inserted, ID: " + result.insertId);
--- db_insert_multiple.js
+++ db_insert_multiple.js
@@ -11,22 +11,11 @@ con.connect(function (err){
     if (err) throw err;
     console.log("Connected!");
 
-    var sql = "INSERT INTO customers (name, address) VALUES ?";
+    var sql = "INSERT INTO products (name) VALUES ?";
     var values = [
-        ['John', 'Highway 71'],
-        ['Peter', 'Lowstreet 4'],
-        ['Amy', 'Apple st 652'],
-        ['Hannah', 'Mountain 21'],
-        ['Michael', 'Valley 345'],
-        ['Sandy', 'Ocean blvd 2'],
-        ['Betty', 'Green Grass 1'],
-        ['Richard', 'Sky st 331'],
-        ['Susan', 'One way 98'],
-        ['Vicky', 'Yellow Garden 2'],
-        ['Ben', 'Park Lane 38'],
-        ['William', 'Central st 954'],
-        ['Chuck', 'Main Road 989'],
-        ['Viola', 'Sideway 1633']
+        ['154'],
+        ['155'],
+        ['156']
     ];
 
     con.query(sql, [values], function (err, result) {
